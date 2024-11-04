2c088103d711abf1ec8384033410cfb64b6a98fd
Matheus Magalhães <matheusop806@gmail.com>
2024-11-03 23:21:09 -0300

    "Finished tests"

--- cypress/e2e/report_problem_test.spec.js
+++
@@ -1,6 +0,0 @@
-describe('Testando página incial do PI2', () => {
-  it('Deve carregar a página inicial', () => {
-    cy.visit('http://localhost:8080'); // Substitua com a URL do seu servidor local
-    cy.contains('Localidades afetadas na cidade').should('be.visible');
-     });
-});
