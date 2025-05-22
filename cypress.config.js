73a85b2c599498b730535c90a8d4f9f91ad178cb
Matheus Magalhães <matheusop806@gmail.com>
2025-05-21 23:18:12 -0300

    "Adjusts in cypress.config.js"

--- cypress.config.js
+++ cypress.config.js
@@ -3,10 +3,9 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // Configurações de eventos, se necessário
     },
-    baseUrl: 'http://localhost:8080', // Rota inicial do projeto
-    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}', // Padrão para localizar os testes
-    supportFile: 'cypress/support/e2e.js', // Arquivo de suporte, se necessário
+    baseUrl: 'http://localhost:8080', 
+    specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
+    supportFile: false
   },
 });
