1302d350ff34ed4785b231c3b09c9b0c433fd298
Matheus Magalhães <matheusop806@gmail.com>
2025-05-22 01:21:51 -0300

    lousiana

--- server.js
+++ server.js
@@ -15,6 +15,6 @@ app.get('/:id', (req, res) => {
 });
 
 // Iniciar o servidor
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
